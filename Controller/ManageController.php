1ddb6e753c1470c0dc3b0ab37983e0ccd012c752
Kevin Papst <kpapst@gmx.net>
2024-01-02 18:50:14 +0100

    import from correct namespace

--- Controller/ManageController.php
+++ Controller/ManageController.php
@@ -21,7 +21,7 @@ use KimaiPlugin\SharedProjectTimesheetsBundle\Repository\SharedProjectTimesheetR
 use KimaiPlugin\SharedProjectTimesheetsBundle\Service\ManageService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route(path: '/shared-project-timesheets')]
--- Controller/ViewController.php
+++ Controller/ViewController.php
@@ -16,7 +16,7 @@ use KimaiPlugin\SharedProjectTimesheetsBundle\Repository\SharedProjectTimesheetR
 use KimaiPlugin\SharedProjectTimesheetsBundle\Service\ViewService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(path: '/auth/shared-project-timesheets')]
 class ViewController extends AbstractController
